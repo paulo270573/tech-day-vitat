dffde0c3f90a1b3dc80f6b5795fe1959d846bdcb
Arthur Coutinho <arthurcoutinho@MacBook-Pro-de-Arthur.local>
2021-09-10 10:25:45 -0300

    Pacote controller

--- src/main/java/br/com/vitat/instigate/controller/Controller.java
+++ src/main/java/br/com/vitat/instigate/controller/Controller.java
@@ -1,4 +1,4 @@
-package br.com.vitat.instigate;
+package br.com.vitat.instigate.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
