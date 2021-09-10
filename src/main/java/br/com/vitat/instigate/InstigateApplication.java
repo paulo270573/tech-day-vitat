66d67f6b6c1ab4b1ca9605c8cc7c97527b7f7c1e
Arthur Coutinho <arthurcoutinho@MacBook-Pro-de-Arthur.local>
2021-09-10 10:23:50 -0300

    Cria projeto

---
+++ src/main/java/br/com/vitat/instigate/InstigateApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitat.instigate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstigateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstigateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitat/instigate/InstigateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitat.instigate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstigateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
